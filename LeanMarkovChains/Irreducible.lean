e521de9df787a8ca6402214389305daceefe411c
chirprush <rush040507@gmail.com>
2026-01-02 18:44:51 -0500

    Proved `MarkovChain.irreducible_same_periods` and `MarkovChain.IsAperiodic.irreducible_period_one_implies_aperiodic`

--- LeanMarkovChains/Irreducible.lean
+++ LeanMarkovChains/Irreducible.lean
@@ -8,14 +8,28 @@ variable {α : Type u} [Fintype α] [DecidableEq α]
 namespace MarkovChain
 
 def Reachable (M : MarkovChain α) (x y : α) :=
-  ∃ k > 0, (M.P ^ k) x y > 0
+  ∃ k > 0, 0 < (M.P ^ k) x y
 
 def IsIrreducible (M : MarkovChain α) :=
-  ∀ x y : α, ∃ k > 0, (M.P ^ k) x y > 0
+  ∀ x y : α, ∃ k > 0, 0 < (M.P ^ k) x y
 
 theorem irreducible_iff_reachable (M : MarkovChain α) :
   M.IsIrreducible ↔ ∀ x y : α, M.Reachable x y := by
   rfl
+
+theorem path_prob_le_all
+  (M : MarkovChain α) (s t : ℕ) (x y z : α) :
+  ((M.P ^ s) x y) * ((M.P ^ t) y z) ≤ (M.P ^ (s + t)) x z := by
+  rw [pow_add, Matrix.mul_apply]
+  have h1 : ∀ j, 0 ≤ (M.P ^ s) x j * (M.P ^ t) j z := by
+    intro j
+    apply mul_nonneg
+    · exact (Matrix.IsNonnegative.pow M.nonneg) x j
+    · exact (Matrix.IsNonnegative.pow M.nonneg) j z
+  exact Finset.single_le_sum
+    (f := fun j => (M.P ^ s) x j * (M.P ^ t) j z)
+    (by intro i hi; simp_rw [h1])
+    (Finset.mem_univ y)
 
 theorem reachable_transitive
   {M : MarkovChain α} {x y z : α}
@@ -29,19 +43,8 @@ theorem reachable_transitive
   use s + t
   constructor
   · exact add_pos hs.left ht.left
-  · rw [pow_add, Matrix.mul_apply]
-    have h0 : 0 < (M.P ^ s) x y * (M.P ^ t) y z := by
+  · have h0 : 0 < (M.P ^ s) x y * (M.P ^ t) y z := by
       apply mul_pos hs.right ht.right
-    have h1 : ∀ j, 0 ≤ (M.P ^ s) x j * (M.P ^ t) j z := by
-      intro j
-      apply mul_nonneg
-      · exact (Matrix.IsNonnegative.pow M.nonneg) x j
-      · exact (Matrix.IsNonnegative.pow M.nonneg) j z
-    have h2 : ∑ j, (M.P ^ s) x j * (M.P ^ t) j z ≥ (M.P ^ s) x y * (M.P ^ t) y z := by
-      exact Finset.single_le_sum
-        (f := fun j => (M.P ^ s) x j * (M.P ^ t) j z)
-        (by intro i hi; simp_rw [h1])
-        (Finset.mem_univ y)
-    linarith [h0, h1, h2]
+    linarith [h0, MarkovChain.path_prob_le_all M s t x y z]
 
 end MarkovChain
--- LeanMarkovChains/Period.lean
+++ LeanMarkovChains/Period.lean
@@ -1,4 +1,5 @@
 import Mathlib
+import Mathlib.Data.Nat.GCD.Basic
 import LeanMarkovChains.Basic
 import LeanMarkovChains.Irreducible
 
@@ -8,12 +9,59 @@ variable {α : Type u} [Fintype α] [DecidableEq α]
 
 namespace MarkovChain
 
+-- Mathlib.NumberTheory.FrobeniusNumber is very helpful here.
+-- We eventually need Schur's theorem for proving that an irreducible, aperiodic
+-- chain is ergodic
+def PeriodStates (M : MarkovChain α) (x : α) :=
+  { t : ℕ | 0 < t ∧ 0 < (M.P ^ t) x x }
+
 noncomputable def Period (M : MarkovChain α) (x : α) :=
-  sInf { t : ℕ | t ≥ 1 ∧ (M.P ^ t) x x > 0 }
+  Nat.setGcd (M.PeriodStates x)
+
+lemma reachable_period_dvd {M : MarkovChain α} {x y : α}
+  (hTo : M.Reachable x y) (hFrom : M.Reachable y x) :
+  M.Period x ∣ M.Period y := by
+  unfold MarkovChain.Period
+  rw [Nat.dvd_setGcd_iff]
+  intro m hm
+  unfold MarkovChain.Reachable at hTo
+  obtain ⟨i, hi⟩ := hTo
+  unfold MarkovChain.Reachable at hFrom
+  obtain ⟨j, hj⟩ := hFrom
+  -- First, show that the gcd divides i + j so that it divides i + j + m
+  have h1 : i + j ∈ M.PeriodStates x := by
+    unfold MarkovChain.PeriodStates
+    constructor
+    · linarith
+    · have hP : 0 < ((M.P ^ i) x y) * ((M.P ^ j) y x) := by
+        exact mul_pos hi.right hj.right
+      linarith [hP, MarkovChain.path_prob_le_all M i j x y x]
+  rw [Nat.dvd_add_iff_right (Nat.setGcd_dvd_of_mem h1)]
+  -- But then we can show that i + j + m is inside the set so we win
+  have h2 : i + j + m ∈ M.PeriodStates x := by
+    unfold MarkovChain.PeriodStates
+    constructor
+    · linarith
+    · have hP1 : 0 < ((M.P ^ i) x y) * ((M.P ^ m) y y) := by
+        unfold MarkovChain.PeriodStates at hm
+        exact mul_pos hi.right hm.right
+      have hP2 : 0 < (M.P ^ (i + m)) x y := by
+        linarith [hP1, MarkovChain.path_prob_le_all M i m x y y]
+      have hP3 : 0 < ((M.P ^ (i + m)) x y) * ((M.P ^ j) y x) := by
+        exact mul_pos hP2 hj.right
+      conv =>
+        arg 2
+        arg 2
+        rw [add_right_comm]
+      linarith [hP2, MarkovChain.path_prob_le_all M (i + m) j x y x]
+  exact Nat.setGcd_dvd_of_mem h2
 
 theorem irreducible_same_periods {M : MarkovChain α} (h : M.IsIrreducible) (x y : α) :
   M.Period x = M.Period y := by
-  sorry
+  rw [MarkovChain.irreducible_iff_reachable] at h
+  exact Nat.dvd_antisymm
+    (MarkovChain.reachable_period_dvd (h x y) (h y x))
+    (MarkovChain.reachable_period_dvd (h y x) (h x y))
 
 noncomputable def IsAperiodic (M : MarkovChain α) :=
   ∀ x : α, M.Period x = 1
@@ -21,6 +69,10 @@ noncomputable def IsAperiodic (M : MarkovChain α) :=
 theorem IsAperiodic.irreducible_period_one_implies_aperiodic {M : MarkovChain α}
   (hIrred : M.IsIrreducible) (h : ∃ x, M.Period x = 1) :
   M.IsAperiodic := by
-  sorry
+  obtain ⟨x, hx⟩ := h
+  unfold MarkovChain.IsAperiodic
+  intro y
+  rw [← hx]
+  exact MarkovChain.irreducible_same_periods hIrred y x
 
 end MarkovChain
